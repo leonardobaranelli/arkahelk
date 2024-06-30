22ec62836d9d6b6d0584df209165ba4641fc3ecb
leo <leobaranelli@gmail.com>
2024-06-30 10:29:10 -0300

    feat(nestjs-backend): shipments, connectors and users resources via nest cli

--- nestjs-react-version/backend/src/app.module.ts
+++ nestjs-react-version/backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ShipmentsModule } from './shipments/shipments.module';
+import { ConnectorsModule } from './connectors/connectors.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ShipmentsModule, ConnectorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
